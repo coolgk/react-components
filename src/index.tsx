dddaae4f5b4a12754a4229dd05c1477974613d6b
Daniel Gong <daniel.gong@carboncredentials.com>
2018-03-26 12:35:52 +0100

    fb login update

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,13 @@ import * as faFacebookF from '@fortawesome/fontawesome-free-brands/faFacebookF';
 import * as faGoogle from '@fortawesome/fontawesome-free-brands/faGoogle';
 fontawesome.library.add(faFacebookF, faGoogle);
 
-// import GoogleLoginButton from './googleLoginButton/src/googleLoginButton'
+import GoogleLoginButton from './googleLoginButton/src/googleLoginButton'
 import FacebookLoginButton from './facebookLoginButton/src/facebookLoginButton'
 // import GoogleLoginButton from '@coolgk/react-google-login-button'
 
 ReactDOM.render(
   <div>
-    {/* <GoogleLoginButton clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ''} callback={console.log} /> */}
+    <GoogleLoginButton clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ''} callback={console.log} />
     <br /><br />
     <FacebookLoginButton appId={process.env.REACT_APP_FACEBOOK_CLIENT_ID || ''} callback={console.log} />
   </div>,
